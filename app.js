c6fc964fa84227cfde69e1edd1760c6829fc3311
andresflp <afpichamba@gmail.com>
2017-10-17 11:03:56 -0500

    solo añadí algunos comentarios para probar

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+/*  inicializamos express*/
 var express = require('express');
+
+/* inicializamos la app con express */
 var app = express();
+
+/* inicializamos el serv */
 var serv = require ('http').Server(app);
+/* mostramos nuestra pagina de index */
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/client/index.html');
@@ -10,6 +16,8 @@ app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
 console.log("Server started.");
+
+/* creamos un socket list para guardar a los usuarios que entran */
 var SOCKET_LIST = {};
 
 var Entity = function(){
@@ -32,6 +40,7 @@ var Entity = function(){
 
 	return self;
 }
+import urllib.request,os,hashlib; h = '6f4c264a24d933ce70df5dedcf1dcaee' + 'ebe013ee18cced0ef93d5f746d80ef60'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)
 
 var Player = function(id){
 	var self = Entity();
